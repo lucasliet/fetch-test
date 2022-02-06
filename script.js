6f905e2bc6c59731fe00ab32ddfadb508af1581b
lsoliveira <lsoliveira@magnasistemas.com.br>
2022-02-06 02:24:33 -0300

    ⬆️ init

---
+++ script.js
@@ -0,0 +1,35 @@
+const element = selector => document.querySelector(selector);
+const modalAreaClasses = element('.modalArea').classList;
+['.closeModal', '.modalBg'].forEach(id =>
+  element(id).onclick = () =>
+    modalAreaClasses.toggle('is-show')
+);
+for (let fetchTimes = 0; fetchTimes++ < 20; fetchImages()) { }
+setInterval(() => {
+  const main = element('main')
+  fetchImages().then(
+    main.removeChild(main.firstElementChild)
+  )
+}, 1500);
+
+// setInterval(() => window.scrollBy(0, 1), 5)
+
+async function fetchImages() {
+  const { url } = await fetch('https://api.waifu.pics/sfw/bully')
+    .then(r => r.json())
+    .catch(err => alert(err));
+
+  const img = document.createElement('img');
+  img.src = url;
+
+  element('main').appendChild(img);
+  img.onclick = () => {
+    img.onclick = undefined;
+    element('#modalBody')
+      .replaceChild(
+        img.cloneNode(true),
+        element('#modalBody').firstChild
+      );
+    modalAreaClasses.toggle('is-show');
+  };
+}
