80d7e4cf058d01c250f951021f9bb0c02221ec5a
lsoliveira <lsoliveira@magnasistemas.com.br>
2022-02-06 18:16:19 -0300

    🖼  center header info + 📱 Mobile responsiviness

--- script.js
+++ script.js
@@ -1,36 +1,57 @@
-const element = selector => document.querySelector(selector);
-const modalAreaClasses = element('.modalArea').classList;
-['.modalBg'].forEach(id =>
-  element(id).onclick = () =>
-    modalAreaClasses.toggle('is-show')
-);
-for (let fetchTimes = 0; fetchTimes++ < 20; fetchImages()) { }
-setInterval(() => {
-  const main = element('main')
-  fetchImages().then(
-    main.removeChild(main.firstElementChild)
-  )
-}, 1500);
+const apiUrl = 'https://api.waifu.pics/sfw/bully';
 
-// setInterval(() => window.scrollBy(0, 1), 5)
+const getElement = selector => document.querySelector(selector);
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+const modalAreaClasses = getElement('.modalArea').classList;
+const header = getElement('header');
+
+['.closeModal', '.modalBg']
+  .map(selector => getElement(selector))
+  .forEach(element =>
+    element.onclick = event => {
+      if (element === event.target)
+        modalAreaClasses.toggle('is-show')
+    }
+  );
+
+header.onclick = () => {
+  header.classList.toggle('topHeader');
+  header.classList.toggle('centerHeader');
+};
+
+console.time('preFetchImagesCall');
+for (let fetchTimes = 0; fetchTimes++ <= 32; fetchImages())
+  console.timeLog('preFetchImagesCall');
+console.timeEnd('preFetchImagesCall');
+
+const updateDom = setInterval(async () => {
+  const main = getElement('main');
+  const img = main.firstChild;
+  await fetchImages();
+  img.style.transition = 'ease-in-out .2s';
+  img.style.opacity = 0;
+  await sleep(200)
+  main.removeChild(img)
+}, 2000);
+
+const stop = () => clearInterval(updateDom);
 
 async function fetchImages() {
-  const { url } = await fetch('https://api.waifu.pics/sfw/bully')
+  const { url } = await fetch(apiUrl)
     .then(r => r.json())
-    .catch(err => alert(err));
+    .catch(err => {
+      alert(`${err}\nvamos parar de atualizar as imagens :(`);
+      stop();
+    });
 
   const img = document.createElement('img');
   img.src = url;
 
   img.onclick = () => {
-    const elClone = img.cloneNode(true)
-    elClone.onclick = undefined;
-    element('#modalBody')
-    .replaceChild(
-      elClone,
-      element('#modalBody').firstChild
-      );
-      modalAreaClasses.toggle('is-show');
-    };
-    element('main').appendChild(img);
-  }
+    getElement('.modalContent>img').src = url;
+    modalAreaClasses.toggle('is-show');
+  };
+  
+  getElement('main').appendChild(img);
+}
